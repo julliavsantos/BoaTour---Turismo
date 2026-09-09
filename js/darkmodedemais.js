f2b532ff60433a77d72e7beb9c131aaa430c021d
Jullia Volpato <volpatosantosjullia@gmail.com>
2026-09-09 16:46:21 -0300

    darkmode cadastro

--- js/darkmodedemais.js
+++ js/darkmodedemais.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
         botao.innerHTML = "☀️";
 
         if (logo) {
-            logo.src = "img/logo/logodarkmode.png";
+            logo.src = "../img/logo/logodarkmode.png";
         }
 
     } else {
